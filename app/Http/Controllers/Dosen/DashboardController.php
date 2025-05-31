6b67fdc61b015d5ab7d3a6fac867c89ce17e123e
ranggaptr <rangga2000putera@gmail.com>
2025-05-31 09:15:09 +0700

    UPDATE: placement controller

---
+++ app/Http/Controllers/Dosen/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Mahasiswa;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\LowonganMagangModel;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $activeMenu = 'dashboard';
+        $lowongans = LowonganMagangModel::with('perusahaan')->latest()->get();
+        return view('roles.dosen.dashboard', compact('activeMenu', 'lowongans'));
+    }
+}

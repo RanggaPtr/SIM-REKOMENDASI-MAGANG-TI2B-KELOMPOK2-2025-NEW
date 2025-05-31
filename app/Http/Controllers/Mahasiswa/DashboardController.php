4270b0c9b9465c4105769f07f2061f7c91a49f4c
Ekya1411 <ekyamuhammad@gmail.com>
2025-05-31 15:43:00 +0700

    fix: merge error

--- app/Http/Controllers/Mahasiswa/DashboardController.php
+++ app/Http/Controllers/Mahasiswa/DashboardController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Mahasiswa;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\LowonganMagangModel;
+use App\Models\PeriodeMagangModel;
+use App\Models\SkemaModel;
+use Carbon\Carbon;
 
 class DashboardController extends Controller
 {
@@ -12,6 +15,13 @@ class DashboardController extends Controller
     {
         $activeMenu = 'dashboard';
         $lowongans = LowonganMagangModel::with('perusahaan')->latest()->get();
-        return view('roles.mahasiswa.dashboard', compact('activeMenu', 'lowongans'));
+
+        // Ambil periode yang belum lewat
+        $periodes = PeriodeMagangModel::where('tanggal_selesai', '>=', Carbon::today())->get();
+
+        // Ambil semua skema (jika ingin tampilkan juga)
+        $skemas = SkemaModel::all();
+
+        return view('roles.mahasiswa.dashboard', compact('activeMenu', 'lowongans', 'periodes', 'skemas'));
     }
 }
